69e9e8ca8b73d28adefc23262faa6e7159313c2f
dmitrii sh <dmitrii@sh.com>
2022-11-25 18:22:43 +0300

    init

---
+++ src/main/java/ru/edu/FirstSecurityApp/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package ru.edu.FirstSecurityApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+
+}
