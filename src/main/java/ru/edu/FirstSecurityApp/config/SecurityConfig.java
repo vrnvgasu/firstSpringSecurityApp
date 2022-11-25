1a34cc7ffa508e2eae9d216f96d0252f20c7711a
dmitrii sh <dmitrii@sh.com>
2022-11-25 23:48:11 +0300

    81 Кастомная страница авторизации

--- src/main/java/ru/edu/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/edu/FirstSecurityApp/config/SecurityConfig.java
@@ -3,13 +3,14 @@ package ru.edu.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.edu.FirstSecurityApp.services.PersonDetailsService;
 
-@EnableWebSecurity	// конфиг для Spring Security
+@EnableWebSecurity  // конфиг для Spring Security
 // в version 3.0.0 WebSecurityConfigurerAdapter - вырезали
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -41,6 +42,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder getPasswordEncoder() {
 		// пока не шифруем
 		return NoOpPasswordEncoder.getInstance();
+	}
+
+	// конфигурируем сам Spring Security: какая страница отвечает за вход, за ошибку и тд
+	// тут также идет управление авторизацией. Кто что может делать
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+
+				// пока не передаем csrf токен с фронта
+				// отключаем защиту от межсайтовой подделки запросов
+				.csrf().disable()
+
+				// настройка прав на просмотр ендпоинтов
+				.authorizeRequests()
+				.antMatchers("/auth/login", "/error").permitAll() // на страницу аутентификации и ошибки пускам всех
+				.anyRequest().authenticated() // на все остальное пускаем только аутентифицированных
+
+				.and() // соединяет разные части настроек
+
+				// процесс аутентификации
+				.formLogin().loginPage("/auth/login") // настраиваем форму для входа (ендпоинт)
+				.loginProcessingUrl("/process_login")  // ендпоинт, куда придет логин и пароль. Его даже реализовывать не надо
+				.defaultSuccessUrl("/hello", true) // перенаправление после успешной аутентификации
+				.failureUrl("/auth/login?error "); // перенаправление при ошибке аутентификации
 	}
 
 }
